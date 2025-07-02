8244419be5767896f18fc0e9804eb0735a5615c0
Sakis_Bog <thanbogiann@gmail.com>
2025-07-02 18:12:46 +0300

    Add reusable Button component and update FeedbackForm
    Introduced a new Button component with customizable props for version, type, and disabled state. Updated FeedbackForm to use the new Button component instead of a standard button element.

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
 import Card from './shared/Card'
+import Button from './Button'
 
 const FeedbackForm = () => {
     const [text,setText]=useState('');
@@ -16,7 +17,7 @@ const FeedbackForm = () => {
             {/* @todo - rating select component */}
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
-                <button type='submit'>Send</button>
+                <Button type='submit'>Send</Button>
             </div>
         </form>
     </Card>
