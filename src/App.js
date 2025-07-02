a79c58a848a01fb2653e0e650205c774f27511e1
Sakis_Bog <thanbogiann@gmail.com>
2025-07-02 17:50:58 +0300

    Add feedback form, stats, and delete functionality
    Introduced FeedbackForm and FeedbackStats components to the app. Updated FeedbackList and FeedbackItem to support deleting feedback items, including a delete button with an icon. Added 'react-icons' dependency for icon support.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React from 'react'
 import {useState} from 'react';
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure...')){
+        setFeedback(feedback.filter((item) =>{
+          return item.id !== id
+        }));
+    };
+  };
+
   return (
     <>
     <Header/>
     <div className='container'>
-      <FeedbackList feedback={feedback} /> 
+      <FeedbackForm />
+      <FeedbackStats feedback={feedback} />
+      <FeedbackList feedback={feedback} 
+      handleDelete={deleteFeedback}/> 
     </div>
     </>
   )
