a40408ed12fdf1ec6240321ecbc1475b7876e036
Sakis_Bog <thanbogiann@gmail.com>
2025-07-04 16:40:35 +0300

    Add React Router and enhance About page
    Integrated React Router for navigation between the main feedback page and the About page. Updated AboutPage to use the Card component and provide project details.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Card from '../components/shared/Card'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <Card>
+        <div>
+            <h1>About this project</h1>
+            <p>this a react app to leave feedback for a product</p>
+            <p>Version: 1.0.0</p>
+            <p>
+                <a href=''>Back To Home</a>
+            </p>
+        </div>
+    </Card>
   )
 }
 
